83826bbae392ac2acc2d0303625251d060924ed7
Natlan <juampilosano99@gmail.com>
2023-07-17 00:31:59 -0300

    actualizacion 17/7/2023

--- Registro/Script.js
+++ Registro/Script.js
@@ -8,6 +8,16 @@ let cSharp = document.getElementById("C#")
 let C = document.getElementById("C++")
 let body = document.querySelector("body")
 
+let pythonDiv = document.getElementById("pythonDiv")
+let javascriptDiv = document.getElementById("javascriptDiv")
+let cDiv = document.getElementById("c++Div")
+let cSharpDiv = document.getElementById("c#Div")
+
+let pythonSpan = document.querySelector("#pythonDiv span")
+let pythonObject = document.querySelector("#pythonDiv object")
+let javascriptSpan = document.querySelector("#javascriptDiv span")
+let javascriptObject = document.querySelector("#javascriptDiv object")
+
 var total = 4
 var count = 0
 
@@ -47,7 +57,17 @@ C.addEventListener("load", ()=>{
 })
 
 window.addEventListener("load", ()=>{
-    setTimeout(()=>{body.style.backgroundColor = "black"; picture.style.opacity = 0}, 2000)
+    setTimeout(()=>{
+        body.style.backgroundColor = "black"; 
+        picture.style.opacity = 0;
+        body.style.animationName = "scrollAnimation"
+        pythonDiv.style.transform = "scale(1)"; pythonDiv.style.top = "-20%"
+        setTimeout(()=>{javascriptDiv.style.transform = "scale(1)"; javascriptDiv.style.top = "-20%"}, 50)
+        setTimeout(()=>{cDiv.style.transform = "scale(1)"; cDiv.style.top = "-20%"}, 100)
+        setTimeout(()=>{cSharpDiv.style.transform = "scale(1)"; cSharpDiv.style.top = "-20%"}, 150)
+        setTimeout(()=>{body.style.animationName = "none"}, 2000)
+        setTimeout(()=>{body.style.animationName = "none"; window.scrollTo({left: 465})}, 2000)
+    }, 2000)
 })
 
 let python_p = document.getElementById("python_p")
@@ -80,46 +100,123 @@ function typed(mensaje, elemento1, elemento2, iterador = 0){
         }
     },1)
 }
-python.addEventListener("mouseover", function (){
-    typed(mensaje, python_p, pythonB, i)
-    pythonB.textContent = "▮"
-})
 
-python.addEventListener("mouseout", ()=>{
+function mouseoverPython(){typed(mensaje, python_p, pythonB, i); pythonB.textContent = "▮"}
+
+pythonDiv.addEventListener("mouseover", mouseoverPython)
+
+pythonDiv.addEventListener("mouseout", ()=>{
     clearInterval(intervalo)
     python_p.textContent = ""
     pythonB.textContent = ""
 })
 
-JavaScript.addEventListener("mouseover", ()=>{
-    typed(mensajeJ, javascript_p, javascriptB, i)
-    javascriptB.textContent = "▮"
-})
+function mouseoverJavascript(){typed(mensajeJ, javascript_p, javascriptB, i); javascriptB.textContent = "▮"}
 
-JavaScript.addEventListener("mouseout", ()=>{
+javascriptDiv.addEventListener("mouseover",  mouseoverJavascript)
+
+javascriptDiv.addEventListener("mouseout", ()=>{
     clearInterval(intervalo)
     javascript_p.textContent = ""
     javascriptB.textContent = ""
 })
 
-C.addEventListener("mouseover", ()=>{
-    typed(mensajeC, cP, cB, i)
-    cB.textContent = "▮"
-})
+function mouseoverC(){typed(mensajeC, cP, cB, i); cB.textContent = "▮"}
 
-C.addEventListener("mouseout", ()=>{
+cDiv.addEventListener("mouseover", mouseoverC)
+
+cDiv.addEventListener("mouseout", ()=>{
     clearInterval(intervalo)
     cP.textContent = ""
     cB.textContent = ""
 })
 
-cSharp.addEventListener("mouseover", ()=>{
-    typed(mensajeSharp, cSharpP, cSharpB, i)
-    cSharpB.textContent = "▮"
-})
+function mouseoverCsharp(){typed(mensajeSharp, cSharpP, cSharpB, i); cSharpB.textContent = "▮"}
 
-cSharp.addEventListener("mouseout", ()=>{
+cSharpDiv.addEventListener("mouseover", mouseoverCsharp)
+
+cSharpDiv.addEventListener("mouseout", ()=>{
     clearInterval(intervalo)
     cSharpP.textContent = ""
     cSharpB.textContent = ""
 })
+
+
+function javascriptClick(){
+    javascriptDiv.style.transform = "scale(1)"
+    javascriptSpan.style.height = "170vh"
+    javascriptSpan.style.width = "190vw"
+    JavaScript.style.height = "130vh"
+    JavaScript.style.width = "100vh"
+    JavaScript.style.top = "20%"
+    JavaScript.style.left = "0"
+    window.scrollTo({left: 300, behavior: "smooth"})
+    body.style.overflowX = "hidden"
+    javascriptObject.style.width = "190vw"
+    javascriptObject.style.height = "150vh"
+    pythonDiv.style.zIndex = "40"
+    pythonDiv.style.top = "40%"
+    pythonDiv.style.left = "50%"
+    pythonDiv.style.transform = "scale(.3)"
+    setTimeout(()=>{cDiv.style.top = "40%"; cDiv.style.left = "-5.5%"; cDiv.style.transform = "scale(.3)"}, 50)
+    setTimeout(()=>{cSharpDiv.style.top = "40%"; 
+    cSharpDiv.style.left = "-20.5%"; 
+    cSharpDiv.style.transform = "scale(.3)"}, 100)
+    pythonDiv.removeEventListener("mouseover", mouseoverPython)
+    cDiv.removeEventListener("mouseover", mouseoverC)
+    cSharpDiv.removeEventListener("mouseover", mouseoverCsharp)
+}
+function javascriptClickInClick(){
+    javascriptDiv.style.transform = "scale(1)"
+    javascriptDiv.style.top = "-20%"
+    javascriptDiv.style.left = "-15%"
+    javascriptSpan.style.height = "170vh"
+    javascriptSpan.style.width = "190vw"
+    JavaScript.style.height = "130vh"
+    JavaScript.style.width = "100vh"
+    JavaScript.style.top = "20%"
+    JavaScript.style.left = "0"
+    javascriptObject.style.width = "190vw"
+    javascriptObject.style.height = "150vh"
+    pythonSpan.style.height = "55vh"
+    pythonSpan.style.width = "45vh"
+    python.style.width = "40vh"
+    python.style.height = "50vh"
+    python.style.inset = "0 0"
+    pythonObject.style.height = "50vh"
+    pythonObject.style.width = "40vh"
+    pythonDiv.style.zIndex = "40"
+    pythonDiv.style.top = "40%"
+    pythonDiv.style.left = "30.5%"
+    pythonDiv.style.transform = "scale(.3)"
+}
+
+pythonDiv.addEventListener("click", ()=>{
+    pythonDiv.style.transform = "scale(1)"
+    pythonSpan.style.height = "170vh"
+    pythonSpan.style.width = "190vw"
+    python.style.height = "130vh"
+    python.style.width = "100vh"
+    python.style.top = "20%"
+    python.style.left = "12%"
+    window.scrollTo({left: "0", behavior: "smooth"})
+    body.style.overflowX = "hidden"
+    pythonObject.style.width = "190vw"
+    pythonObject.style.height = "150vh"
+    javascriptDiv.style.top = "40%"
+    javascriptDiv.style.left = "-10%"
+    javascriptDiv.style.transform = "scale(.3)"
+    setTimeout(()=>{cDiv.style.top = "40%"; cDiv.style.left = "-25%"; cDiv.style.transform = "scale(.3)"}, 50)
+    setTimeout(()=>{cSharpDiv.style.top = "40%"; 
+    cSharpDiv.style.left = "-40%"; 
+    cSharpDiv.style.transform = "scale(.3)"}, 100)
+    javascriptDiv.removeEventListener("mouseover", mouseoverJavascript)
+    cDiv.removeEventListener("mouseover", mouseoverC)
+    cSharpDiv.removeEventListener("mouseover", mouseoverCsharp)
+    pythonDiv.removeEventListener("mouseover", mouseoverPython)
+
+    javascriptDiv.removeEventListener("click", javascriptClick)
+    javascriptDiv.addEventListener("click", javascriptClickInClick)
+})
+
+javascriptDiv.addEventListener("click", javascriptClick)
